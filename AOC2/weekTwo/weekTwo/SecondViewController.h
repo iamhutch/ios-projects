28148f854e739cad223825ed9121b569fd294919
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-10-12 22:58:47 -0500

    Moving from mdv-projects to ios-projects.

---
+++ AOC2/weekTwo/weekTwo/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  weekTwo
+//
+//  Created by Lucy Hutcheson on 10/3/12.
+//  Copyright (c) 2012 Lucy Hutcheson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+// Set up my IBAction to close the second view
+-(IBAction)onClose:(id)sender;
+
+@end
